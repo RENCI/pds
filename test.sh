2c81a624f608d4b9c9c698d07150705d7b046dcd
Hao Xu <xuh@cs.unc.edu>
2019-11-01 13:23:34 -0400

    update

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose -f docker-compose.yml -f test/docker-compose.yml -f test/pds-server/docker-compose.yml up --build -V --exit-code-from pdsaggregator
+docker-compose -f docker-compose.yml -f test/docker-compose.yml -f test/pds-server/docker-compose.yml up --build -V --exit-code-from pdsaggregator-test
