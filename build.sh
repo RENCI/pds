bf93681af6bb5e07095f78dfceb5d6286f42b3ca
Hao Xu <xuh@cs.unc.edu>
2019-11-01 12:27:04 -0400

    update

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-docker build . -t pds-aggregator:$VERSION
-docker tag pds-aggregator:0.1.0 zooh/pds-aggregator:$VERSION
-docker push zooh/pds-aggregator:0.1.0
+if [ -z $1 ]; then
+    COMMIT_MSG=update
+else
+    COMMIT_MSG=$1
+fi
+docker build . -t $IMAGE:$VERSION
+docker tag $IMAGE:0.1.0 zooh/$IMAGE:$VERSION
+docker push zooh/$IMAGE:0.1.0
 git add -u
-git commit -m "update"
+git commit -m $COMMIT_MSG
 git tag v$VERSION -f
 git push
 git push --tags -f
