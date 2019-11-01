fd566601fca40e35d9ca5e39aabc54cb4d1234d6
Hao Xu <xuh@cs.unc.edu>
2019-11-01 12:31:25 -0400

    update

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 if [ -z $COMMIT_MSG ]; then
     COMMIT_MSG=update
 fi
+if [ -z $IMAGE ]; then
+    echo environment variable IMAGE not set
+    exit 1
+fi
+if [ -z $VERSION ]; then
+    echo environment variable VERSION not set
+    exit 1
+fi
 docker build . -t $IMAGE:$VERSION
 docker tag $IMAGE:0.1.0 zooh/$IMAGE:$VERSION
 docker push zooh/$IMAGE:0.1.0
