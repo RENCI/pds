6d72f189a7dfddb8a21c79f23e56f56d4b5870af
Hao Xu <xuh@cs.unc.edu>
2020-02-17 10:42:58 -0500

    add docker-compose file

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-docker-compose --env-file test/env.docker -f docker-compose.yml -f test/docker-compose.yml -f test/pds-server/docker-compose.yml up --build -V --exit-code-from pdsaggregator-test
+docker-compose --env-file test/env.docker -f docker-compose.yml -f test/docker-compose.yml -f test/docker-compose.mock.yml up --build -V --exit-code-from pdsaggregator-test
 
 TX_TAG=`git describe --exact-match --tags $(git log -n1 --pretty='%h')  2> /dev/null; `                                                                      
 if [ "${TX_TAG}" == "" ] ; then TX_TAG='unstable'; fi                                                                                                        
