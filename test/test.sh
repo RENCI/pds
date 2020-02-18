79de06fa631265ca0a604fec8e344ecdec6ab3c3
Hao Xu <xuh@cs.unc.edu>
2020-02-18 17:39:59 -0500

    remove env file

--- test/test.sh
+++ test/test.sh
@@ -6,6 +6,6 @@ source env.TAG
 source test/env.docker
 set +o allexport
 
-docker-compose --env-file test/env.docker -f docker-compose.yml -f test/docker-compose.yml -f test/docker-compose.mock.yml up --build -V --exit-code-from pdsaggregator-test
+docker-compose -f docker-compose.yml -f test/docker-compose.yml -f test/docker-compose.mock.yml up --build -V --exit-code-from pdsaggregator-test
 
 # docker-compose --env-file test/env.docker -f docker-compose.yml -f test/docker-compose.system.yml -f test/pds-server-system/docker-compose.yml up --build -V --exit-code-from pdsaggregator-test
