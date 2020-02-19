8785116e924d06719277fd3d3bb9287dc0563802
Hao Xu <xuh@cs.unc.edu>
2020-02-19 18:04:12 -0500

    copy TAG to build dir

--- test.system/deploy.sh
+++ test.system/deploy.sh
@@ -14,18 +14,21 @@ fi
 
 cp -r test.system/tx-router $build
 cp -r test.system/plugin $build/tx-router
+cp env.TAG $build
 cp test.system/env.pds $build/tx-router/env.txrouter
 cp test.system/docker-compose.system.yml $build/tx-router/test
 cp test.system/Dockerfile.system $build/tx-router/test
 cp test.system/test_func.system.py $build/tx-router/test
 
-cd $build/tx-router
+cd $build
 
 set -o allexport
 source env.TAG
-source test/env.docker
+source tx-router/test/env.docker
 INIT_PLUGIN_PATH=./plugin
 set +o allexport
+
+cd tx-router
 
 MONGO_INITDB_ROOT_PASSWORD=example MONGO_NON_ROOT_PASSWORD=collection JWT_SECRET=secret docker-compose -f docker-compose.yml -f nginx/unsecure/docker-compose.yml up --build -V -t 3000
 
