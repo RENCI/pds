77efaffb244ce485df3b0363251c94c6aea45254
Hao Xu <xuh@cs.unc.edu>
2020-02-19 18:53:33 -0500

    refactor deploy.sh

--- test.system/test.system.sh
+++ test.system/test.system.sh
@@ -14,21 +14,26 @@ fi
 
 cp -r test.system/tx-router $build
 cp -r test.system/plugin $build/tx-router
+cp env.TAG $build
 cp test.system/env.pds $build/tx-router/env.txrouter
 cp test.system/docker-compose.system.yml $build/tx-router/test
 cp test.system/Dockerfile.system $build/tx-router/test
 cp test.system/test_func.system.py $build/tx-router/test
 
-cd $build/tx-router
+cd $build
 
 set -o allexport
 source env.TAG
-source test/env.docker
+source tx-router/test/env.docker
 INIT_PLUGIN_PATH=./plugin
 set +o allexport
 
-# MONGO_INITDB_ROOT_PASSWORD=example MONGO_NON_ROOT_PASSWORD=collection JWT_SECRET=secret docker-compose -f docker-compose.yml -f nginx/unsecure/docker-compose.yml up --build -V -t 3000
+cd tx-router
 
-MONGO_INITDB_ROOT_PASSWORD=example MONGO_NON_ROOT_PASSWORD=collection JWT_SECRET=secret docker-compose -f docker-compose.yml -f nginx/unsecure/docker-compose.yml -f test/docker-compose.system.yml up --build -V -t 3000 --exit-code-from=pdsaggregator-test
+if [ "$1" == "deploy" ]; then
+    MONGO_INITDB_ROOT_PASSWORD=example MONGO_NON_ROOT_PASSWORD=collection JWT_SECRET=secret docker-compose -f docker-compose.yml -f nginx/unsecure/docker-compose.yml up --build -V -t 3000 -d
+else
+    MONGO_INITDB_ROOT_PASSWORD=example MONGO_NON_ROOT_PASSWORD=collection JWT_SECRET=secret docker-compose -f docker-compose.yml -f nginx/unsecure/docker-compose.yml -f test/docker-compose.system.yml up --build -V -t 3000 --exit-code-from=pdsaggregator-test
+fi
 
 
