085e6288286417bfea8297d0047826d78ec18333
rajshruti18 <47569410+rajshruti18@users.noreply.github.com>
2020-07-22 18:34:43 -0400

    Field names updated to use camelCase

--- test.system/test_func.system.py
+++ test.system/test_func.system.py
@@ -128,7 +128,7 @@ json_post_headers={
 }
 
 def test_get_pds_patient_variables():
-    resp = requests.post(f"http://localhost:8080/v1/plugin/pds/patientVariables", json={"ptid": "smart-7321938", "guidance_piid": "pdspi-guidance-example", "timestamp": "2020-02-19T00:00:00Z"}, headers=json_post_headers)
+    resp = requests.post(f"http://localhost:8080/v1/plugin/pds/patientVariables", json={"ptid": "smart-7321938", "guidancePiid": "pdspi-guidance-example", "timestamp": "2020-02-19T00:00:00Z"}, headers=json_post_headers)
 
     print(resp.content)
     assert resp.status_code == 200
@@ -153,7 +153,7 @@ def test_get_pds_patient_variables():
     ]
 
 def test_get_pds_patient_variables_no_timestamp():
-    resp = requests.post(f"http://localhost:8080/v1/plugin/pds/patientVariables", json={"ptid": "smart-7321938", "guidance_piid": "pdspi-guidance-example"}, headers=json_post_headers)
+    resp = requests.post(f"http://localhost:8080/v1/plugin/pds/patientVariables", json={"ptid": "smart-7321938", "guidancePiid": "pdspi-guidance-example"}, headers=json_post_headers)
 
     print(resp.content)
     assert resp.status_code == 200
